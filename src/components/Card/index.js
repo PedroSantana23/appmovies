f626262bc77a9461600782b4fd33a80bd058e1f8
Pedro Henrique <ljitdpdf@gmail.com>
2024-05-28 15:01:37 -0300

    Aula 17

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,8 +2,14 @@ import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 import iconFavorite from "./favorite.png";
 import iconUnFavorite from "./unfavorite.png";
+import { useFavoriteContext } from "../../contexts/Favorites";
 
 function Card({ id }) {
+
+    const { favorite, addFavorite } = useFavoriteContext()
+    const isFavorite = favorite.some((fav) => fav.id === id)
+    const icone = isFavorite ? iconUnFavorite : iconFavorite
+
     return (
         <section className={styles.card}>
             <Link to={`/watch/${id}`} >
@@ -11,7 +17,11 @@ function Card({ id }) {
                 alt="Capa" className={styles.capa}/>
             </Link>
             <figure className={styles.icon}>
-                <img src={iconFavorite} alt="Ícone" />
+                <img 
+                    src={icone} 
+                    alt="Ícone" 
+                    onClick={() => addFavorite({id})}    
+                />
             </figure>
         </section>
     )
